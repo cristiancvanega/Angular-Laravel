5db3fb9770e74e58ac1a2e7fbfa5a8f993a21ec4
Cristian Castro <cristian1853@gmail.com>
2016-02-04 20:36:12 -0500

    Bug fixed into UserRoutes

--- app/Http/Routes/UserRoutes.php
+++ app/Http/Routes/UserRoutes.php
@@ -1,5 +1,5 @@
 <?php
 Route::post('create_user','userController@create');
 Route::put('update_user','userController@update');
-Route::delete('delete_user','userController@delete')
+Route::delete('delete_user','userController@delete');
 Route::get('search_user/{id}','userController@search');
