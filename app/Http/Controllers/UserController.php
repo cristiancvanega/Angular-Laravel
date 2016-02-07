35b6f9703c5b097929986ab16aec809fb339f824
Cristian Castro <cristian1853@gmail.com>
2016-02-07 12:24:00 -0500

    Generated endpoint for create a new User, and filter request with class Request

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,21 +6,27 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Obsan\Entities\User;
+use Illuminate\Routing\Controller;
+use App\Http\Requests\UserCreateRequest;
 
-class UserController extends BaseController
+class UserController extends Controller
 {
 
     protected $user;
 
     public function __construct(User $user)
     {
-        parent::__construct();
         $this->user = $user;
-
     }
 
     public function find($id)
     {
         return $this->user->find($id);
+    }
+
+    public function create(UserCreateRequest $request)
+    {
+        \Log::info($request);
+        return $this->user->create($request->toArray());
     }
 }
--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -3,8 +3,15 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Symfony\Component\HttpFoundation\Response;
 
 abstract class Request extends FormRequest
 {
-    //
+    public function response(array $errors)
+    {
+        if(count($errors) > 0)
+        {
+            return response()->json($errors, 400);
+        }
+    }
 }
