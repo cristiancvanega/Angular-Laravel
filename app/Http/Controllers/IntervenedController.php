f9f76aacf3d20698bd8db54ab40e74c4c926ee16
Cristian Castro <cristian1853@gmail.com>
2016-02-08 23:36:57 -0500

    Added Update, Delete, Create Intervened

--- app/Http/Controllers/IntervenedController.php
+++ app/Http/Controllers/IntervenedController.php
@@ -8,6 +8,7 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Obsan\Entities\Intervened;
 use App\Http\Requests\IntervenedCreateRequest;
+use App\Http\Requests\IntervenedUpdateRequest;
 
 class IntervenedController extends Controller
 {
--- app/Http/Requests/IntervenedUpdateRequest.php
+++ app/Http/Requests/IntervenedUpdateRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use App\Http\Requests\Request;
 
-class IntervenedCreateRequest extends Request
+class IntervenedUpdateRequest extends Request
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,9 +24,9 @@ class IntervenedCreateRequest extends Request
     public function rules()
     {
         return [
-            'name'          => 'required|string',
-            'document_type' => 'required|numeric',
-            'document'      => 'required|numeric',
+            'name'          => 'string',
+            'document_type' => 'numeric',
+            'document'      => 'numeric',
             'address'       => 'string',
             'phone'         => 'numeric',
             'email'         => 'email|unique:users,email',
