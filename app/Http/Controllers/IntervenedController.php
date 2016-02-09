fe950cb5a372f9ffe021b823af4da6773b003ffc
Cristian Castro <cristian1853@gmail.com>
2016-02-08 22:56:44 -0500

    Before merge with master branch

--- app/Http/Controllers/IntervenedController.php
+++ app/Http/Controllers/IntervenedController.php
@@ -14,4 +14,17 @@ class IntervenedController extends Controller
     {
         $this->model = $intervened;
     }
+
+    public function create(IntervenedCreateRequest $request)
+    {
+        return $this->responseEntityStore($this->model->create($request->toArray()));
+    }
+
+    public function update(IntervenedUpdateRequest $request, $id)
+    {
+        $u = $this->model->find($id);
+        if(is_null($u))
+            return response()->json(['User does not exist'], 400);
+        return response()->json($u->update($request->toArray()), 202);
+    }
 }
