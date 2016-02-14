1d94d0818583e712a2bac2f821fc8f9a541c9d9f
Cristian Castro <cristian1853@gmail.com>
2016-02-14 12:05:57 -0500

    Added custom report for Intervention

--- app/Http/Routes/InterventionRoutes.php
+++ app/Http/Routes/InterventionRoutes.php
@@ -1,4 +1,4 @@
-<?php
+`<?php
 
 Route::group(['prefix' => 'intervention'], function(){
 
--- app/Http/Routes/ReportRoutes.php
+++ app/Http/Routes/ReportRoutes.php
@@ -1,8 +1,18 @@
 <?php
 
+//Route::group(['prefix' => 'report', 'middleware' => 'obsan'], function(){
 Route::group(['prefix' => 'report'], function(){
 
     Route::get('intervention', 'InterventionController@getData');
 
     Route::get('evaluation', 'EvaluationController@getReportData');
+});
+
+Route::group(['prefix' => 'report/custom_report', 'middleware' => 'admin'], function(){
+
+    Route::post('intervened', 'IntervenedController@getCustomReport');
+
+    Route::post('evaluation', 'EvaluationController@getCustomReport');
+
+    Route::post('intervention', 'InterventionController@getCustomReport');
 });
