8d72a82c002d1807928f1df060cced2cd26bf262
Cristian Castro <cristian1853@gmail.com>
2016-02-13 23:30:05 -0500

    Create intervened_intervention row before create intervention for relationship with intervened

--- app/Obsan/Repositories/InterventionRepository.php
+++ app/Obsan/Repositories/InterventionRepository.php
@@ -3,13 +3,24 @@ namespace App\Obsan\Repositories;
 
 
 use App\Obsan\Entities\Entity;
+use App\Obsan\Entities\IntervenedIntervention;
 use App\Obsan\Entities\Intervention;
 
 class InterventionRepository extends BaseRepository
 {
-    public function __construct(Intervention $intervention)
+    protected $intervenedIntervention;
+
+    public function __construct(Intervention $intervention, IntervenedIntervention $intervenedIntervention)
     {
         parent::__construct($intervention);
+        $this->intervenedIntervention = $intervenedIntervention;
+    }
+
+    public function create($array)
+    {
+        $i = $this->model->create($array);
+        $this->intervenedIntervention->create(['interventions_id' => $i->id, 'intervened_id' => $array['intervened_id']]);
+        return $i;
     }
 
     public function getData()
