266aed607c3c7fb83b0ca4a710ac4216cd729871
Cristian Castro <cristian1853@gmail.com>
2016-02-14 08:41:42 -0500

    BaseRole

--- app/Http/Middleware/BaseRole.php
+++ app/Http/Middleware/BaseRole.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Middleware;
 
+use App\Obsan\Repositories\UserRepository;
 use Closure;
 
 class BaseRole
 {
+    protected $userRepository;
+
+    public function __construct(UserRepository $repository)
+    {
+        $this->userRepository = $repository;
+    }
+
     /**
      * Handle an incoming request.
      *
@@ -16,5 +24,15 @@ class BaseRole
     public function handle($request, Closure $next)
     {
         return $next($request);
+    }
+
+    protected function isAdmin()
+    {
+        return $this->userRepository->getRoleUser() == 'Admin';
+    }
+
+    protected function isObsan()
+    {
+        return $this->userRepository->getRoleUser() == 'Obsan';
     }
 }
