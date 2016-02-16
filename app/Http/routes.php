eaee510322ddf321b63ea29d05404855db8ad6ec
Cristian Castro <cristian1853@gmail.com>
2016-02-15 19:57:21 -0500

    Added some routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,10 +5,13 @@ Route::get('/', function () {
     return redirect(env('obsan-web'));
 });
 
+Route::get('/obsan-web/index', function () {
+    return redirect('obsan-web/index.html');
+});
+
 Route::get('/obsan-web/obsan', function () {
     return redirect('obsan-web/index_uso.html');
 });
-
 
 Route::get('/obsan-web/admin', function () {
     return redirect('obsan-web/index_usa.html');
