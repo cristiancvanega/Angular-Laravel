c2f9006e0a3d9d328d607701c4b41ce077c63fd1
Cristian Castro <cristian1853@gmail.com>
2016-02-17 00:18:26 -0500

    Added custom report for intervention

--- public/obsan-web/js/controllers.js
+++ public/obsan-web/js/controllers.js
@@ -406,6 +406,7 @@ app.controller("customReportIntervention", [
 
         $scope.customReport = function()
         {
+            console.log($scope.registroConsultarIntervencion.name);
             datos = {
                 entity_id               : $scope.registroConsultarIntervencion.intervention_id,
                 municipality_id         : $scope.registroConsultarIntervencion.municipality_id,
