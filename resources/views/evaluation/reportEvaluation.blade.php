439fcb1a3cf4789856edab1f643df0c3b2c64699
Cristian Castro <cristian1853@gmail.com>
2016-02-20 15:06:18 -0500

    Added template for generate report evaluation

---
+++ resources/views/evaluation/reportEvaluation.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Evaluaciones</title>
+</head>
+<body>
+<table>
+    <head>
+        <tr>
+            <th>Intervencion</th>
+            <th>Usuario</th>
+            <th>Descripci&oacute;n Evidencia</th>
+            <th>Impacto</th>
+            <th>Estado Inicial</th>
+            <th>Estado Final</th>
+            <th>Descripci&oacute;n</th>
+            <th>Recomendaciones</th>
+        </tr>
+    </head>
+    <tbody>
+    @foreach($evaluation as $item)
+        <tr>
+            <td>{{$item->intervention}}</td>
+            <td>{{$item->user}}</td>
+            <td>{{$item->descripcion_evidencia}}</td>
+            <td>{{$item->impacto}}</td>
+            <td>{{$item->estado_inicial}}</td>
+            <td>{{$item->estado_final}}</td>
+            <td>{{$item->description}}</td>
+            <td>{{$item->recomendaciones}}</td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
+</body>
+</html>
