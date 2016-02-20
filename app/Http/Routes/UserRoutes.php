7ca03653a05c63572e2a76f8d95fc8e42d731dce
Cristian Castro <cristian1853@gmail.com>
2016-02-20 15:56:31 -0500

    Added endpoint to consume getIDEmaiAdmins, and routes refactor

--- app/Http/Routes/UserRoutes.php
+++ app/Http/Routes/UserRoutes.php
@@ -1,14 +1,24 @@
 <?php
+Route::group(['prefix' => 'user'], function(){
 
-Route::group(['prefix' => 'user', 'middleware' => 'admin'], function()
-{
-	Route::get('','UserController@all');
+	Route::group(['middleware' => 'admin'], function()
+	{
+		Route::get('','UserController@all');
 
-	Route::get('{id}','UserController@find');
+		Route::get('{id}','UserController@find');
 
-	Route::post('','UserController@create');
+		Route::post('','UserController@create');
 
-	Route::put('{id}','UserController@update');
+		Route::put('{id}','UserController@update');
 
-	Route::delete('/{id}','UserController@delete');
+		Route::delete('/{id}','UserController@delete');
+		
+	});
+
+	Route::group(['middleware' => 'obsan'], function(){
+
+		Route::get('id_email_admins', 'UserController@getIdEmailAdmins');
+
+	});
+
 });
