2e2c9c94609f569445dc7c9c0bc2d5b694c60d6c
cristiancvanega <cristian1853@gmail.com>
2016-02-28 11:46:45 -0500

    Added filter for date into InterventionRepository for customReport

--- app/Obsan/Repositories/IntervenedRepository.php
+++ app/Obsan/Repositories/IntervenedRepository.php
@@ -5,6 +5,7 @@ namespace App\Obsan\Repositories;
 
 use App\Obsan\Entities\Intervened;
 use App\Obsan\Entities\Intervention;
+use Carbon\Carbon;
 
 class IntervenedRepository extends BaseRepository
 {
@@ -24,6 +25,22 @@ class IntervenedRepository extends BaseRepository
 
     public function getCustomReportI($intervention_id)
     {
+        //dd(Carbon::parse($intervention_id['date'])->year);
+
+        if(isset($intervention_id['date']))
+        {
+            $date = Carbon::parse($intervention_id['date'])->year;
+            dd($date);
+            return (new Intervention())->with('intervened')
+                ->where('start_date', '=', Carbon::parse($intervention_id['date'])->year)->get();
+        }
+
+        if(isset($intervention_id['intervention_id']))
+             dd($intervention_id['intervention_id']);
+
+        if(isset($intervention_id['date']))
+            dd($intervention_id['date']);
+
         return (new intervention)->with('intervened')->find($intervention_id['intervention_id'])->toArray()['intervened'];
     }
 }
