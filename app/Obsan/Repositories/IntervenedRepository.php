2a1941cf4dc53b791e724472f0b80681811e2ea1
cristiancvanega <cristian1853@gmail.com>
2016-03-01 20:09:16 -0500

    Bug fixed into interventionRepository in customReport

--- app/Obsan/Repositories/IntervenedRepository.php
+++ app/Obsan/Repositories/IntervenedRepository.php
@@ -25,22 +25,6 @@ class IntervenedRepository extends BaseRepository
 
     public function getCustomReportI($intervention_id)
     {
-        //dd(Carbon::parse($intervention_id['date'])->year);
-
-        if(isset($intervention_id['date']))
-        {
-            $date = Carbon::parse($intervention_id['date'])->year;
-            dd($date);
-            return (new Intervention())->with('intervened')
-                ->where('start_date', '=', Carbon::parse($intervention_id['date'])->year)->get();
-        }
-
-        if(isset($intervention_id['intervention_id']))
-             dd($intervention_id['intervention_id']);
-
-        if(isset($intervention_id['date']))
-            dd($intervention_id['date']);
-
         return (new intervention)->with('intervened')->find($intervention_id['intervention_id'])->toArray()['intervened'];
     }
 }
